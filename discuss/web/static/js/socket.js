ef1642288987c05d57bede215340531b869b4cd9
Daniel Torres <denial.torres@gmail.com>
2024-11-24 13:06:50 -0600

    websockets, interst comments and get the response in the collection

--- discuss/web/static/js/socket.js
+++ discuss/web/static/js/socket.js
@@ -5,9 +5,20 @@ let socket = new Socket("/socket", {params: {token: window.userToken}})
 
 socket.connect()
 
-let channel = socket.channel("comments:1", {})
-channel.join()
-  .receive("ok", resp => { console.log("Joined successfully", resp) })
-  .receive("error", resp => { console.log("Unable to join", resp) })
 
-export default socket
+const createSocket = (topicId) => {
+  let channel = socket.channel(`comments:${topicId}`, {})
+
+  channel.join()
+    .receive("ok", resp => { console.log("Joined successfully", resp) })
+    .receive("error", resp => { console.log("Unable to join", resp) })
+
+  document.querySelector('button').addEventListener('click', () => {
+    const content = document.querySelector('textarea').value;
+
+    channel.push('comment:add', { content: content });
+  });
+}
+
+
+window.createSocket = createSocket;
